3aed342b59244b271e3875bb0315598e5e504ca1
ryannicholas <ryannicholas.vieira@gmail.com>
2022-09-12 15:49:31 -0300

    dist removed from repository, jwt validation added

--- backend/src/middlewares/authValidation/getUserData.ts
+++ backend/src/middlewares/authValidation/getUserData.ts
@@ -2,15 +2,26 @@ import { Request, Response, NextFunction } from "express";
 import { findSession } from "../../repositories/sessionRepository";
 import { checkError } from "../errorHandler";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default async function getUserData(req:Request, res:Response, next:NextFunction){
-    const token  = req.headers['x-access-token'].toString();
+    const token = req.headers['x-access-token'].toString();
 
     if(!token) throw checkError(401, "You must send authorization token!");
 
+    const SECRET = process.env.JWT_SECRET;
+
+    try {
+        jwt.verify(token,SECRET);
+    } catch (error) {
+        return res.status(401).send("Invalid token");
+    }
+    
     const session = await findSession(token);
     
-    if(!session) throw checkError(401, "This token is invalid or has expired!");
+    if(!session) throw checkError(401, "This token has expired!");
 
     const userInfo = {
         token,
